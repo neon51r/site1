f18b727d3ca9b848193695024e7ef8ab54e60757
PavelTarasov <neon51r@mail.ru>
2017-01-04 15:19:16 +0300

    dytc нерн внес

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html>
 <head>
 	<meta charset="utf-8" /> 
-	<title>Добавление контакта</title>
+	<title>Добавление контакта тукт</title>
 	<link rel="stylesheet" type="text/css" href="main.css" media="all">
 </head>
 <body>
